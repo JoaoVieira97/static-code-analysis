548008b536658f3e80f7bd4085fc340045266ef4
SimaoBarbosa <SimaoBarbosa@users.noreply.github.com>
2019-05-01 19:03:19 +0100

    Script funccionalityRules with files to test

---
+++ Script_FuncionalityRules_byF3M/Controllers/UtentesController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using F3M.UMinho.Esocial.Units.Data.F3MESR3S1Domain;
+using F3M.UMinho.Esocial.Units.Data.F3MESR3S1Repositories;
+using F3M.UMinho.Esocial.Units.Data.F3MESR3S1RepositoriesInterfaces;
+using F3M.UMinho.Esocial.Units.Data.F3MRadRepositories;
+using F3M.UMinho.Esocial.Units.Data.F3MRadRepositoriesInterfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace F3M.UMinho.Esocial.Units.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UtentesController : ControllerBase
+    {
+        readonly IUtentesRepository _UtentesRepository;
+        readonly IEntidadesRegistadasRepository _EntidadesRegistadasRepository;
+        readonly IEntidadesRepository _EntidadesRepository;
+        readonly IRADCabecalhoRepository _IRADCabecalhoRepository;
+
+        public UtentesController(IUtentesRepository UtentesRepository, IEntidadesRegistadasRepository EntidadesRegistadasRepository, IEntidadesRepository EntidadesRepository, IRADCabecalhoRepository IRADCabecalhoRepository)
+        {
+            _UtentesRepository = UtentesRepository;
+            _EntidadesRegistadasRepository = EntidadesRegistadasRepository;
+            _EntidadesRepository = EntidadesRepository;
+            _IRADCabecalhoRepository = IRADCabecalhoRepository;
+        }
+
+        // GET api/utentes/
+        [HttpGet]
+        public async Task<List<object>> Get()
+        {
+            return await _UtentesRepository.Get(_UtentesRepository, _EntidadesRegistadasRepository, _EntidadesRepository);
+        }
+
+        // GET api/utentes/5
+        [HttpGet("{id}")]
+        public async Task<List<object>> Get(long id)
+        {
+            return await _UtentesRepository.Get(_UtentesRepository, _EntidadesRegistadasRepository, _EntidadesRepository, id);
+        }
+
+        // GET api/utentes/quarto/5
+        [HttpGet("quarto/{id}")]
+        public async Task<List<object>> GetByQuarto(int id)
+        {
+            return await _UtentesRepository.GetByQuarto(_UtentesRepository, _EntidadesRegistadasRepository, _EntidadesRepository, _IRADCabecalhoRepository, id);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Script_FuncionalityRules_byF3M/script2.py
@@ -0,0 +1,75 @@
+import fileinput
+import re
+import sys
+import os
+
+tables = []
+models = []
+reps = []
+controllers = []
+def readTables():
+    tables = []
+    for line in fileinput.input(files=sys.argv[1]):
+        if ("public" in line and "virtual" in line and "DbSet" in line and "get" in line and "set" in line):
+            splitLine = re.split(r'\s+', line)
+            tables.append(splitLine[4])
+    return tables
+
+def readFilesFromPath(path):
+  models = []
+  modelsPath = path
+  models = (os.listdir(modelsPath))
+  return models
+
+def readAll():
+  global tables
+  global models
+  global reps
+  global controllers 
+  tables = readTables()
+  print("Tables :" + str(tables))
+  models = readFilesFromPath(sys.argv[2])
+  print("Models :" + str(models))
+  reps = readFilesFromPath(sys.argv[3])
+  print("Reps :" + str(reps))
+  controllers = readFilesFromPath(sys.argv[4])
+  print("Controllers :" + str(controllers))
+
+
+def testModels():
+  missing = len(tables)
+  for t in tables :
+    for m in models :
+      t_withcs = t[2:]+".cs" # TbClient = Client.cs to compare with model
+      if( t_withcs == m) :
+        missing-=1
+  return missing
+
+def testReps():
+  missing = len(tables)
+  for t in tables :
+    for r in reps :
+      t_withcs = "Repositorio" + t[2:] + ".cs"  # TbClient = RepositorioClient.cs to compare with Repository
+      if( t_withcs == r) :
+        missing-=1
+  return missing
+
+def testControllers():
+  missing = len(tables)
+  for t in tables :
+    for c in controllers :
+      t_withcs = t[2:]+"Controller.cs"  # TbClient = ClientController.cs to compare with Controller
+      if( t_withcs == c) :
+        missing-=1
+  return missing
+
+def testAll():
+  modelsMissing = testModels()
+  print("Missing "+ str(modelsMissing) +" from " + str(len(tables)) + " models according to tables!")
+  repsMissing = testReps()
+  print("Missing "+ str(repsMissing) +" from " + str(len(tables)) + " repositorys according to tables!")
+  controllersMissing = testControllers()
+  print("Missing "+ str(controllersMissing) +" from " + str(len(tables)) + " controllers according to tables!")
+
+readAll()
+testAll()
