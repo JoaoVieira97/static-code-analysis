99ac268d4b696ad068fb0260f746e5007e5def25
JoaoVieira97 <jonasv55@hotmail.com>
2019-05-01 23:27:56 +0100

    Update on script 2: presentation and storing missing data

--- Script_FuncionalityRules_byF3M/script2.py
+++ Script_FuncionalityRules_byF3M/script2.py
@@ -1,80 +1,102 @@
+#!/usr/bin/env python3
+
 import fileinput
 import re
 import sys
 import os
 
+# readable info
 tables = []
 models = []
 reps = []
 controllers = []
+
+# missing files
+models_m = []
+reps_m = []
+controllers_m = []
+
 def readTables():
-    tables = []
+    global tables
     for line in fileinput.input(files=sys.argv[1]):
         if ("public" in line and "virtual" in line and "DbSet" in line and "get" in line and "set" in line):
             splitLine = re.split(r'\s+', line)
             tables.append(splitLine[4])
-    return tables
 
 def readFilesFromPath(path):
-  models = []
-  modelsPath = path
-  models = (os.listdir(modelsPath))
-  return models
+  files = []
+  files = (os.listdir(path))
+  return files
 
 def readAll():
   global tables
   global models
   global reps
-  global controllers 
-  tables = readTables()
-  print("Tables :" + str(tables))
+  global controllers
+  
+  readTables()
+  print('\033[4m' + '\033[1m' + '---> TABLES:' + '\033[0m')
+  print('\n'.join(tables))
+
   models = readFilesFromPath(sys.argv[2])
-  print("Models :" + str(models))
+  print('\033[1m' + '\n---> MODELS:' + '\033[0m')
+  print('\n'.join(models))
+
   reps = readFilesFromPath(sys.argv[3])
-  print("Reps :" + str(reps))
+  print('\033[1m' + '\n---> REPOSITORIES:' + '\033[0m')
+  print('\n'.join(reps))
+  
   controllers = readFilesFromPath(sys.argv[4])
-  print("Controllers :" + str(controllers))
+  print('\033[1m' + '\n--> CONTROLLERS:' + '\033[0m')
+  print('\n'.join(controllers))
 
-
-def testModels():
+# i = 1 -> models
+# i = 2 -> reps
+# i = 3 -> controllers
+def testDirectory(i):
   missing = len(tables)
-  for t in tables :
-    for m in models :
-      t_withcs = t[2:]+".cs" # TbClient = Client.cs to compare with model
-      if( t_withcs == m) :
-        missing-=1
-  return missing
+  
+  aux = []
+  if (i == 1): aux = models
+  elif (i == 2): aux = reps
+  else: aux = controllers
 
-def testReps():
-  missing = len(tables)
-  for t in tables :
-    for r in reps :
-      t_withcs = "Repositorio" + t[2:] + ".cs"  # TbClient = RepositorioClient.cs to compare with Repository
-      if( t_withcs == r) :
-        missing-=1
-  return missing
-
-def testControllers():
-  missing = len(tables)
-  for t in tables :
-    for c in controllers :
-      t_withcs = t[2:]+"Controller.cs"  # TbClient = ClientController.cs to compare with Controller
-      if( t_withcs == c) :
-        missing-=1
+  for t in tables:
+    exist = False
+    for a in aux:
+      if (i == 1): t_withcs = t[2:] + ".cs" # TbClient = Client.cs to compare with model
+      elif (i == 2): t_withcs = "Repositorio" + t[2:] + ".cs" # TbClient = RepositorioClient.cs to compare with Repository
+      else: t_withcs = t[2:] + "Controller.cs" # TbClient = ClientController.cs to compare with Controller
+      if (t_withcs == a):
+        missing -= 1
+        exist = True
+    if (exist == False):
+      if (i == 1): models_m.append(t)
+      elif (i == 2): reps_m.append(t)
+      else: controllers_m.append(t)
+  
   return missing
 
 def testAll():
-  modelsMissing = testModels()
-  print("Missing "+ str(modelsMissing) +" from " + str(len(tables)) + " models according to tables!")
-  repsMissing = testReps()
-  print("Missing "+ str(repsMissing) +" from " + str(len(tables)) + " repositorys according to tables!")
-  controllersMissing = testControllers()
-  print("Missing "+ str(controllersMissing) +" from " + str(len(tables)) + " controllers according to tables!")
+  print('\033[91m' + '\033[1m' + '\n--> RESULTS:' + '\033[0m')
+  
+  modelsMissing = testDirectory(1)
+  print("Missing " + str(modelsMissing) + " from " + str(len(tables)) + " models according to tables!")
+  #print('\n'.join(models_m))
+
+  repsMissing = testDirectory(2)
+  print("Missing " + str(repsMissing) + " from " + str(len(tables)) + " repositorys according to tables!")
+  #print('\n'.join(reps_m))
+
+  controllersMissing = testDirectory(3)
+  print("Missing " + str(controllersMissing) + " from " + str(len(tables)) + " controllers according to tables!")
+  #print('\n'.join(controllers_m))
 
 readAll()
 testAll()
 
 # To test this script use : 
-#   py script2.py TablesContextFile ModelsPath RepositorysPath ControllersPath
+#   $ python script2.py TablesContextFile ModelsPath RepositorysPath ControllersPath
 #   example:
-#     py script2.py  F3MESR3S1Context.cs './Models' './Reps' './Controllers'
+#     $ chmod +x script2.py
+#     $ ./script2.py  F3MESR3S1Context.cs Models Reps Controllers
